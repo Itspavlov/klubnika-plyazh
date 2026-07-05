0261696a183d50575f93ad06cb9c6de2a65d616f
Itspavlov <taras.pavlov.92@mail.ru>
2026-07-05 21:49:29 +0300

    Увеличен лимит запросов до 50mb

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const path = require('path');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static('.'));
 
 // ===== НАСТРОЙКИ =====
